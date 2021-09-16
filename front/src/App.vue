6982c6c7d49066b2556e85f9cdee5397f9d53e23
jnheo1216 <hjeionyng94@gmail.com>
2021-09-16 17:18:52 +0900

    S05P21B106-448 feat: lobby 페이지 틀 잡기

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/1">About</router-link>
   </div>
   <router-view />
 </template>
